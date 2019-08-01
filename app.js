8f06ce4b9584cf8b459cb79c2b720088e84d718f
DimasYudhistira91 <dimasmillaty@gmail.com>
2019-08-01 14:05:34 +0700

    membuat web server

--- app.js
+++ app.js
@@ -1,1 +1,48 @@
-console.log('hello world');
+// INTRO
+
+// function sayHello(nama) {
+//   console.log('Hello ' + nama);
+// }
+
+// sayHello('Dimas');
+
+// console.log(window);
+
+
+// Membuat web server:
+
+// var http = require('http');
+
+// function handleRequest(req, res) {
+//   res.end('Hi, selamat menggunakan node.js');
+// }
+
+// var server = http.createServer(handleRequest);
+
+// server.listen(8000, function() {
+//   console.log('server running on http://localhost:8000');
+// });
+
+
+var http = require('http');
+var fs   = require('fs');
+
+function handleRequest(req, res) {
+  res.writeHead(200, {'Content-Type': 'text-html'});
+
+  fs.readFile('./index.html', null, function(error, data) {
+    if(error) {
+      res.writeHead(404);
+      res.write('File Not Found!');
+    } else {
+      res.write(data);
+    }
+    res.end();
+  });
+}
+
+var server = http.createServer(handleRequest);
+
+server.listen(5000, function() {
+  console.log('server running on http://localhost:5000');
+});
