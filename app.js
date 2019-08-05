c5d75497f6319ca874abbd1b54cc6e54de93cc91
DimasYudhistira91 <dimasmillaty@gmail.com>
2019-08-05 18:59:24 +0700

    belajar menggunakan require

--- app.js
+++ app.js
@@ -24,25 +24,28 @@
 // });
 
 
-var http = require('http');
-var fs   = require('fs');
+// var http = require('http');
+// var fs   = require('fs');
 
-function handleRequest(req, res) {
-  res.writeHead(200, {'Content-Type': 'text-html'});
+// function handleRequest(req, res) {
+//   res.writeHead(200, {'Content-Type': 'text-html'});
 
-  fs.readFile('./index.html', null, function(error, data) {
-    if(error) {
-      res.writeHead(404);
-      res.write('File Not Found!');
-    } else {
-      res.write(data);
-    }
-    res.end();
-  });
-}
+//   fs.readFile('./index.html', null, function(error, data) {
+//     if(error) {
+//       res.writeHead(404);
+//       res.write('File Not Found!');
+//     } else {
+//       res.write(data);
+//     }
+//     res.end();
+//   });
+// }
 
-var server = http.createServer(handleRequest);
+// var server = http.createServer(handleRequest);
 
-server.listen(5000, function() {
-  console.log('server running on http://localhost:5000');
-});
+// server.listen(5000, function() {
+//   console.log('server running on http://localhost:5000');
+// });
+
+const tutorial = require('./tutorial');
+console.log(tutorial);
